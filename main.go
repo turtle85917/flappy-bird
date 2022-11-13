7a5e354fe818eb69c84d379cd496689112450d49
turtle85917 <siu85917@gmail.com>
2022-11-14 06:14:06 +0900

    The background keeps moving

--- main.go
+++ main.go
@@ -14,36 +14,55 @@ const (
 )
 
 var (
-	background = color.RGBA{66, 154, 208, 255}
-	ground     = color.RGBA{73, 53, 12, 255}
-	deepground = color.RGBA{64, 46, 11, 255}
+	background     = color.RGBA{66, 154, 208, 255}
+	deepbackground = color.RGBA{65, 150, 200, 255}
+	ground         = color.RGBA{73, 53, 12, 255}
+	deepground     = color.RGBA{64, 46, 11, 255}
+
+	backgrounds = []Background{}
 
 	grounds      = []Ground{}
 	groundHeight = float64(70)
-	groundX      = float64(0)
+
+	endlessX = float64(0)
 )
 
-type Ground struct {
+type Sprite struct {
 	x float64
 	y float64
 	w float64
 	h float64
 }
 
+type Background struct {
+	Sprite
+}
+
+type Ground struct {
+	Sprite
+}
+
 type Game struct{}
 
 func (g *Game) Update() error {
-	groundX -= 10
-	if groundX <= screenSizeX*-1 {
-		groundX = screenSizeX
+	endlessX -= 10
+	if endlessX <= screenSizeX*-1 {
+		endlessX = screenSizeX
 	}
 
-	groundRefresh()
+	refresh()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(background)
+	for idx := 0; idx < len(backgrounds); idx++ {
+		bclr := deepbackground
+		if idx%2 == 0 {
+			bclr = background
+		}
+		ebitenutil.DrawRect(screen, backgrounds[idx].x, backgrounds[idx].y, backgrounds[idx].w, backgrounds[idx].h, bclr)
+	}
+
 	for idx := 0; idx < len(grounds); idx++ {
 		gclr := ground
 		if idx%2 == 0 {
@@ -58,18 +77,30 @@ func (g *Game) Layout(outsidewidth, outsideheight int) (screenwidth, screenheigh
 	return screenSizeX, screenSizeY
 }
 
-func groundRefresh() {
+func refresh() {
 	grounds = []Ground{
-		{x: screenSizeX*-1 + groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight},
-		{x: groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight},
-		{x: screenSizeX + groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}}
+		{Sprite{x: screenSizeX*-1 + endlessX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}},
+		{Sprite{x: endlessX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}},
+		{Sprite{x: screenSizeX + endlessX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}}}
+	backgrounds = []Background{
+		{Sprite{x: initX(-1), y: 0, w: screenSizeX, h: screenSizeY}},
+		{Sprite{x: initX(0), y: 0, w: screenSizeX, h: screenSizeY}},
+		{Sprite{x: initX(1), y: 0, w: screenSizeX, h: screenSizeY}}}
+}
+
+func initX(level float64) float64 {
+	return screenSizeX*level + endlessX
 }
 
 func init() {
 	grounds = append(grounds,
-		Ground{x: screenSizeX*-1 + groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight},
-		Ground{x: groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight},
-		Ground{x: screenSizeX + groundX, y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight})
+		Ground{Sprite{x: initX(-1), y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}},
+		Ground{Sprite{x: initX(0), y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}},
+		Ground{Sprite{x: initX(1), y: screenSizeY - groundHeight, w: screenSizeX, h: groundHeight}})
+	backgrounds = append(backgrounds,
+		Background{Sprite{x: initX(-1), y: 0, w: screenSizeX, h: screenSizeY}},
+		Background{Sprite{x: initX(0), y: 0, w: screenSizeX, h: screenSizeY}},
+		Background{Sprite{x: initX(1), y: 0, w: screenSizeX, h: screenSizeY}})
 }
 
 func main() {
